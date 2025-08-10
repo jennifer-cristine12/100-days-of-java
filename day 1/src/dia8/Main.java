5eddb982e330c9de90bcbe6e7a77b39b25bf8d59
jennifer-cristine12 <jennifer.cristine11@gmail.com>
2025-08-10 20:14:38 -0300

    dia 8 java.math

---
+++ day 1/src/dia8/Main.java
@@ -0,0 +1,8 @@
+package dia8;
+import dia8.View.*;
+public class Main {
+    public static void main(String[] args) {
+        MathView MV = new MathView();
+        MV.menu();
+    }
+}
---
+++ day 1/src/dia8/model/MathModel.java
@@ -0,0 +1,23 @@
+package dia8.model;
+
+public class MathModel {
+    private int numero1;
+    private float numero2;
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public void setNumero2(float numero2) {
+        this.numero2 = numero2;
+    }
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public float getNumero2() {
+        return numero2;
+    }
+
+}
